6d8178707ac2b093d47b7a5563c9893c66dea0a5
cauematheus <cauematheus_23@hotmail.com>
2024-04-21 18:49:49 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioHipotenusa/Program.cs
@@ -0,0 +1,18 @@
+﻿Double c1, c2, h, hipotenusa;
+do
+{
+    Console.WriteLine("Digite o 1° cateto");
+    c1 = Double.Parse(Console.ReadLine());
+} while (c1 <= 0);
+
+do
+{
+    Console.WriteLine("Digite o 2° cateto");
+    c2 = Double.Parse(Console.ReadLine());
+} while (c2 <= 0);
+
+h  = Math.Sqrt(Math.Pow(c1, 2) + Math.Pow(c2,2));
+
+Console.WriteLine($"A hipotenusa é {h}");
+
+
